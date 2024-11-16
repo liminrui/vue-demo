5dabee7dce6b812c6f7d695e330bec8979fa9b88
Admin <209695793@qq.com>
2024-11-17 07:39:02 +0800

    修改base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: process.env.NODE_ENV === 'production' ? '/vue-demo/' : '/',
+  // base: '', // a empty string
+  base: './', //process.env.NODE_ENV === 'production' ? '/vue-demo/' : '/',
   plugins: [vue()],
 })
